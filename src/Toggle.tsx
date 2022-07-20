aef9898e0d40826825ad855c9d6424d7d221df8d
Your Name <you@example.com>
2022-07-20 13:10:56 +0500

    state management and material ui integration

--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -3,13 +3,24 @@ import { connect } from "react-redux";
 import { IReduxToggleMessageAction, toggleMessage } from "./actions";
 import { Dispatch, bindActionCreators } from "redux";
 import { AppState } from "./rootReducer";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+
+const StyledMaterialButton  = styled(Button)`
+  background-color: ${props => props.theme.colors.secondary};
+  color: white;
+  &:hover {
+    background-color: grey;
+    color: #343e3d;
+  }
+`;
 
 const Toggle: React.FC<ToggleProps> = ({
   messageVisibility,
   toggleMessage
 }) => (
   <div>
-    <button onClick={toggleMessage}>Toggle</button>
+    <StyledMaterialButton onClick={toggleMessage}>Toggle</StyledMaterialButton>
     {messageVisibility && <p>Toggled content</p>}
   </div>
 );
