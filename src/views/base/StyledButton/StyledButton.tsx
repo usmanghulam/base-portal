17aa256b839b6b664c4498b6e83a5bf1c93407b5
Ahmad Butt <87975447+ahmadkbutt@users.noreply.github.com>
2022-07-27 14:11:41 +0500

    Changes: - Base Colors initialized

--- src/views/base/StyledButton/StyledButton.tsx
+++ src/views/base/StyledButton/StyledButton.tsx
@@ -2,9 +2,8 @@ import { Button, darken, styled } from '@mui/material'
 
 const CustomButton = styled(Button)(
   ({ theme }) => `
-  color: ${theme.palette.primary.main};
   :hover {
-    color: ${darken(theme.palette.primary.main, 0.2)};
+    color: ${darken(theme.palette.primary.main, 0.5)};
   }
 `,
 )
